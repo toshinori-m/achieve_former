f7fe01949193786408304c9d8bd20462e9b46e28
toshinori-m <your.email@example.com>
2022-03-07 11:11:04 +0900

    Finish layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -12,61 +12,61 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
     assert_select "title", "#{@base_title}"
   end
 
   test "should get goal" do
-    get static_pages_goal_url
+    get goal_path
     assert_response :success
     assert_select "title", "Goal | #{@base_title}"
   end
 
   test "should get threemonths_goal" do
-    get static_pages_threemonths_goal_url
+    get threemonths_goal_path
     assert_response :success
     assert_select "title", "Threemonths_goal | #{@base_title}"
   end
 
   test "should get monthly_goal" do
-    get static_pages_monthly_goal_url
+    get monthly_goal_path
     assert_response :success
     assert_select "title", "Monthly_goal | #{@base_title}"
   end
 
   test "should get report" do
-    get static_pages_report_url
+    get report_path
     assert_response :success
     assert_select "title", "Report | #{@base_title}"
   end
 
   test "should get training_menu" do
-    get static_pages_training_menu_url
+    get training_menu_path
     assert_response :success
     assert_select "title", "Training_menu | #{@base_title}"
   end
 
   test "should get participating_room" do
-    get static_pages_participating_room_url
+    get participating_room_path
     assert_response :success
     assert_select "title", "Participating_room | #{@base_title}"
   end
 
   test "should get like" do
-    get static_pages_like_url
+    get like_path
     assert_response :success
     assert_select "title", "Like | #{@base_title}"
   end
 
   test "should get participationg_room_user" do
-    get static_pages_participationg_room_user_url
+    get participationg_room_user_path
     assert_response :success
     assert_select "title", "Participationg_room_user | #{@base_title}"
   end
 
   test "should get message" do
-    get static_pages_message_url
+    get message_path
     assert_response :success
     assert_select "title", "Message | #{@base_title}"
   end
