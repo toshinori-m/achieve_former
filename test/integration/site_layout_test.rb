3a6961e033b6325d7e7f5245bfed278f84d0256a
toshinori-m <your.email@example.com>
2022-04-15 18:38:47 +0900

    Add calendar

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -5,7 +5,6 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
-
     assert_select "a[href=?]", training_menu_path
   end
 end
